31effde8b8a3306d4a22ec17432d86d8acd68193
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-06 18:14:44 +0100

    adds folder and user repositories

--- src/main/java/com/codeclan/example/filesandfoldershw/repositories/FolderRepository.java
+++ src/main/java/com/codeclan/example/filesandfoldershw/repositories/FolderRepository.java
@@ -1,4 +1,7 @@
 package com.codeclan.example.filesandfoldershw.repositories;
 
-public class FolderRepository {
+import com.codeclan.example.filesandfoldershw.models.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
 }
--- src/main/java/com/codeclan/example/filesandfoldershw/repositories/UserRepository.java
+++ src/main/java/com/codeclan/example/filesandfoldershw/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.codeclan.example.filesandfoldershw.repositories;
 
-public class UserRepository {
+import com.codeclan.example.filesandfoldershw.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
