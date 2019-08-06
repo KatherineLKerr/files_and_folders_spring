daac2f68cce8bed6dae98b7f7d047a13fa223265
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-06 21:53:06 +0100

    data loader seeds database

--- src/main/java/com/codeclan/example/filesandfoldershw/components/DataLoader.java
+++ src/main/java/com/codeclan/example/filesandfoldershw/components/DataLoader.java
@@ -1,5 +1,8 @@
 package com.codeclan.example.filesandfoldershw.components;
 
+import com.codeclan.example.filesandfoldershw.models.File;
+import com.codeclan.example.filesandfoldershw.models.Folder;
+import com.codeclan.example.filesandfoldershw.models.User;
 import com.codeclan.example.filesandfoldershw.repositories.FileRepository;
 import com.codeclan.example.filesandfoldershw.repositories.FolderRepository;
 import com.codeclan.example.filesandfoldershw.repositories.UserRepository;
@@ -26,7 +29,28 @@ public class DataLoader implements ApplicationRunner {
 
     public void run(ApplicationArguments args) {
 
+        User user1 = new User("katherine");
+        userRepository.save(user1);
 
+        Folder colours = new Folder("colours", user1);
+        folderRepository.save(colours);
 
+        Folder animals = new Folder("animals", user1);
+        folderRepository.save(animals);
+
+        File blue = new File("blue", ".txt", 50, colours);
+        fileRepository.save(blue);
+
+        File orange = new File("orange", ".txt", 40, colours);
+        fileRepository.save(orange);
+
+        File whales = new File("whales", ".txt", 55, animals);
+        fileRepository.save(whales);
+
+        File bears = new File("bears", ".txt", 324, animals);
+        fileRepository.save(bears);
+
+        File giraffes = new File("giraffes", ".txt", 23, animals);
+        fileRepository.save(giraffes);
     }
 }
